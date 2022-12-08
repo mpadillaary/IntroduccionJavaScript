d56f5bfb3a03b36357cf5af69934a3a0274e64b1
Miguel Padilla <miguel.padilla@elkayinteriorsystems.com>
2022-12-08 10:17:17 -0600

    Second commit

--- js/Arreglos.js
+++ js/Arreglos.js
@@ -1,16 +1,23 @@
 //* Arreglos o Arrays
 
-//Arreglos se declaran con corchetes    
+//Arreglos se declaran con corchetes
 const numeros = [10, 20, 30, 40, 50];
 console.table(numeros);
 
-const meses = new Array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo');
+const meses = new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo");
 console.table(meses);
 
-const meses2 = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'];
+const meses2 = ["Enero", "Febrero", "Marzo", "Abril", "Mayo"];
 console.table(meses2);
 
-const arreglo = ['Hola', 10, true, null, {nombre: 'Juan', trabajo: 'Programador'}, [1, 2, 3]];
+const arreglo = [
+  "Hola",
+  10,
+  true,
+  null,
+  { nombre: "Juan", trabajo: "Programador" },
+  [1, 2, 3],
+];
 console.log(arreglo);
 
 //* Acceder a las posiciones de un arreglo
@@ -19,6 +26,6 @@ console.log(arreglo[5][1]);
 //Conocer la extensión de un arreglo
 console.log(meses.length);
 
-numeros.forEach( function(numero) {
-    console.log(numero); 
+numeros.forEach(function (numero) {
+  console.log(numero);
 });
